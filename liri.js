2691c135e22cbd5f8bd03bb673d222f39fe54ae5
Lindsay <lindsay.lbm@gmail.com>
2019-10-15 08:25:59 -0500

    bands and movie default working' :

--- liri.js
+++ liri.js
@@ -1,3 +1,6 @@
+// GLOBAL VARIABLES
+// ============================================================================================================================
+
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
@@ -8,7 +11,7 @@ var spotify = new Spotify(keys.spotify);
 var typeRequest = process.argv[2]
 var dataRequest = process.argv.slice(3).join("+");
 
-
+// FUNCTIONS ==================================================================================================================
 
 function switchFunction() {
 
@@ -38,20 +41,20 @@ function spotifyFunction() {
             console.log("Album Name: " + spotifyResponse.tracks.items[0].album.name);
         })
         .catch(function (err) {
-            console.log(err);
+            spotify.search({ type: 'track', query: "The Sign", limit: 10 })
+                .then(function (spotifyResponse) {
+                    console.log("Artist: " + spotifyResponse.tracks.items[5].album.artists[0].name);
+                    console.log("Song Name: " + spotifyResponse.tracks.items[5].name);
+                    console.log("Preview Url: " + spotifyResponse.tracks.items[5].preview_url);
+                    console.log("Album Name: " + spotifyResponse.tracks.items[5].album.name);
+                })
         })
-}
 
+}
 // Bands in Town Function
 function bandsInTownFunction() {
     var bandsRequest;
-    if (dataRequest !== undefined) {
-        bandsRequest = dataRequest;
-    }
-    // NOT WORKING
-    else {
-        bandsRequest = "Celine Dion"
-    }
+    bandsRequest = dataRequest;
     axios
         .get("https://rest.bandsintown.com/artists/" + bandsRequest + "/events?app_id=codingbootcamp")
         .then(function (bandsResponse) {
@@ -64,21 +67,29 @@ function bandsInTownFunction() {
                 console.log("--------------------------");
             }
         })
-        .catch(function (err) {
-            console.log(err);
-        })
+        .error
+    {
+        bandsRequest = "Celine Dion";
+        axios
+            .get("https://rest.bandsintown.com/artists/" + bandsRequest + "/events?app_id=codingbootcamp")
+            .then(function (bandsResponse) {
+                for (i = 0; i < bandsResponse.data.length; i++) {
+
+                    console.log("Venue: " + bandsResponse.data[0].venue.name);
+                    // console.log(bandsResponse.request._events);
+                    console.log("Venue Location: " + bandsResponse.data[0].venue.city + ", " + bandsResponse.data[0].venue.region);
+                    console.log("Date of Event: " + moment(bandsResponse.data[0].datetime).format('MM/DD/YYYY'));
+                    console.log("--------------------------");
+                }
+            })
+
+    }
 }
 
 // OMDB Function
 function OMDBFunction() {
     var OMDBRequest;
-    if (dataRequest !== undefined) {
-        OMDBRequest = dataRequest;
-    }
-    // NOT WORKING
-    else {
-        OMDBRequest = "Mr. Nobody"
-    }
+    OMDBRequest = dataRequest;
     axios
         .get("http://www.omdbapi.com/?apikey=trilogy&t=" + OMDBRequest)
         .then(function (OMDBResponse) {
@@ -91,6 +102,22 @@ function OMDBFunction() {
             console.log("*Plot: " + OMDBResponse.data.Plot);
             console.log("*Actors: " + OMDBResponse.data.Actors);
         })
+        .error
+    {
+        OMDBRequest = "Mr. Nobody";
+        axios
+            .get("http://www.omdbapi.com/?apikey=trilogy&t=" + OMDBRequest)
+            .then(function (OMDBResponse) {
+                console.log("* Title: " + OMDBResponse.data.Title);
+                console.log("* Year of Release: " + OMDBResponse.data.Year);
+                console.log("* IMDB Rating: " + OMDBResponse.data.Ratings[0].Value);
+                console.log("* Rotton Tomatoes Rating: " + OMDBResponse.data.Ratings[1].Value);
+                console.log("* Country of Production: " + OMDBResponse.data.Country);
+                console.log("*Language: " + OMDBResponse.data.Language);
+                console.log("*Plot: " + OMDBResponse.data.Plot);
+                console.log("*Actors: " + OMDBResponse.data.Actors);
+            })
+    }
 }
 
 // Do what it says function
